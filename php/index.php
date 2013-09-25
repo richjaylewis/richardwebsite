5bb184b77c635ab65edd8158a8e681c2be6b8aa1
richjaylewis <richardlewis1@richards-MacBook-Pro.local>
2013-09-25 23:18:00 +0100

    Files

---
+++ php/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="richardcss.css">	
+		<script src="smooth-scroll.js"></script>
+		<!/http://cferdinandi.github.io/smooth-scroll//>
+		</head>
+	<class="body">
+		<div id="wrap">
+			<div id="leftbar">
+				<div id="nav">
+					<p>
+						Rich </br> 
+						Jay </br>
+						Lewis</br>
+					</p>
+					<p id="portfolio">
+						Portfolio
+					</p>
+					<ul id="links">
+						Adverts
+						Logos
+						Illustration
+						Web Design
+					</p>
+				</div>
+			</div>
+			<div id="rightbar">
+			
+			</div>
+			<div id="bottombar">
+			</div>
+		</div>
+	
+	
+	</body>
+</html>
